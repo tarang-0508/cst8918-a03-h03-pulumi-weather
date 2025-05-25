086ae58a5c2346cb112001071cb6ddafd95a94b7
tarang-0508 <sava0207@algonquinlive.com>
2025-05-25 01:41:50 -0400

    Update index.ts

--- infastructure/index.ts
+++ infastructure/index.ts
@@ -1,1 +1,39 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as azure from "@pulumi/azure-native";
+
+const config = new pulumi.Config();
+const location = config.require("azure-native:location");
+const appName = config.require("weatherAppName");
+
+// Resource Group
+const resourceGroup = new azure.resources.ResourceGroup("weather-rg", {
+  location,
+});
+
+// App Service Plan
+const appServicePlan = new azure.web.AppServicePlan("weather-plan", {
+  location,
+  resourceGroupName: resourceGroup.name,
+  sku: {
+    name: "B1",
+    tier: "Basic",
+  },
+});
+
+// Web App
+const webApp = new azure.web.WebApp("weather-webapp", {
+  location,
+  resourceGroupName: resourceGroup.name,
+  serverFarmId: appServicePlan.id,
+  siteConfig: {
+    linuxFxVersion: "NODE|18-lts",
+  },
+  httpsOnly: true,
+  name: appName,
+});
+
+// Export public URL
+export const url = webApp.defaultHostName.apply(
+  host => `https://${host}`
+);
 
