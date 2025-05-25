e7d112ee40f6ffee174e73fbbdf2dd2deb34b40b
tarang-0508 <sava0207@algonquinlive.com>
2025-05-25 01:46:58 -0400

    Update index.ts

--- infastructure/index.ts
+++ infastructure/index.ts
@@ -1,39 +1,51 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure-native";
 
+// Get config values
 const config = new pulumi.Config();
 const location = config.require("azure-native:location");
 const appName = config.require("weatherAppName");
 
-// Resource Group
+// Create an Azure Resource Group
 const resourceGroup = new azure.resources.ResourceGroup("weather-rg", {
-  location,
+  location: location,
 });
 
-// App Service Plan
+// Create an App Service Plan (Basic Tier)
 const appServicePlan = new azure.web.AppServicePlan("weather-plan", {
-  location,
   resourceGroupName: resourceGroup.name,
+  location: resourceGroup.location,
   sku: {
     name: "B1",
     tier: "Basic",
   },
+  kind: "Linux",
+  reserved: true,
 });
 
-// Web App
+// Create the Web App
 const webApp = new azure.web.WebApp("weather-webapp", {
-  location,
   resourceGroupName: resourceGroup.name,
+  location: resourceGroup.location,
   serverFarmId: appServicePlan.id,
   siteConfig: {
-    linuxFxVersion: "NODE|18-lts",
+    appSettings: [
+      {
+        name: "WEBSITE_RUN_FROM_PACKAGE",
+        value: "1", // This means the app expects a .zip deploy
+      },
+      {
+        name: "NODE_ENV",
+        value: "production",
+      },
+    ],
+    linuxFxVersion: "NODE|18-lts", // Node version for Remix app
   },
   httpsOnly: true,
   name: appName,
 });
 
-// Export public URL
+// ✅ Export the public URL so it shows up in `pulumi stack output`
 export const url = webApp.defaultHostName.apply(
   host => `https://${host}`
 );
-
