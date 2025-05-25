716538723798005b745d6945f3b213ab4c5fb1ce
tarang-0508 <sava0207@algonquinlive.com>
2025-05-25 02:25:08 -0400

    Update index.ts

--- infastructure/index.ts
+++ infastructure/index.ts
@@ -1,49 +1,39 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure-native";
 
-// Get config values
 const config = new pulumi.Config();
 const location = config.require("azure-native:location");
 const appName = config.require("weatherAppName");
 
-// Create an Azure Resource Group
+// Resource Group
 const resourceGroup = new azure.resources.ResourceGroup("weather-rg", {
-  location: location,
+  location,
 });
 
-// Create an App Service Plan (Basic Tier)
+// App Service Plan
 const appServicePlan = new azure.web.AppServicePlan("weather-plan", {
   resourceGroupName: resourceGroup.name,
   location: resourceGroup.location,
+  kind: "Linux",
+  reserved: true,
   sku: {
     name: "B1",
     tier: "Basic",
   },
-  kind: "Linux",
-  reserved: true,
 });
 
-// Create the Web App
+// Web App
 const webApp = new azure.web.WebApp("weather-webapp", {
   resourceGroupName: resourceGroup.name,
   location: resourceGroup.location,
   serverFarmId: appServicePlan.id,
   siteConfig: {
-    appSettings: [
-      {
-        name: "WEBSITE_RUN_FROM_PACKAGE",
-        value: "1", // This means the app expects a .zip deploy
-      },
-      {
-        name: "NODE_ENV",
-        value: "production",
-      },
-    ],
-    linuxFxVersion: "NODE|18-lts", // Node version for Remix app
+    linuxFxVersion: "NODE|18-lts",
   },
   httpsOnly: true,
   name: appName,
 });
+
 
 export const url = webApp.defaultHostName.apply(
   host => `https://${host}`
